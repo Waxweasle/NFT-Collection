d7f3627a4719a8c45f001653e7fd1c509edc42d5
Matthew <103790652+Waxweasle@users.noreply.github.com>
2022-08-25 20:19:18 -0400

    Update CryptoDevs.sol

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -6,17 +6,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IWhitelist.sol";
 
 contract CryptoDevs is ERC721Enumerable, Ownable {
-    /**
-     * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
-     * token will be the concatenation of the `baseURI` and the `tokenId`.
-     */
+
     string _baseTokenURI;
     uint256 public _price = 0.01 ether;
 
     // pause the contract in case of an emergency
     bool public _paused;
 
-    // max supply
     uint256 public maxTokenIds = 20;
 
     // total number minted
@@ -25,10 +21,8 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     // Whitelist contract instance
     IWhitelist whitelist;
 
-    // keep track of whether presale started or not
     bool public presaleStarted;
 
-    // timestamp for when presale would end
     uint256 public presaleEnded;
 
     modifier onlyWhenNotPaused() {
@@ -36,12 +30,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         _;
     }
 
-    /**
-     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
-     * name in our case is `Crypto Devs` and symbol is `CD`.
-     * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
-     * It also initializes an instance of whitelist interface.
-     */
+    
     constructor(string memory baseURI, address whitelistContract)
         ERC721("Crypto Devs", "CD")
     {
@@ -49,17 +38,12 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         whitelist = IWhitelist(whitelistContract);
     }
 
-    /**
-     * @dev startPresale starts a presale for the whitelisted addresses
-     */
+
     function startPresale() public onlyOwner {
         presaleStarted = true;
         presaleEnded = block.timestamp + 5 minutes;
     }
 
-    /**
-     * @dev allows a user to mint one NFT per transaction during the presale.
-     */
     function presaleMint() public payable onlyWhenNotPaused {
         require(
             presaleStarted && block.timestamp < presaleEnded,
@@ -72,15 +56,9 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         require(tokenIds < maxTokenIds, "Exceeded maximum Crypto Devs supply");
         require(msg.value >= _price, "Ether sent is not correct");
         tokenIds += 1;
-        //_safeMint is a safer version of the _mint function as it ensures that
-        // if the address being minted to is a contract, then it knows how to deal with ERC721 tokens
-        // If the address being minted to is not a contract, it works the same way as _mint
+       
         _safeMint(msg.sender, tokenIds);
     }
-
-    /**
-     * @dev mint allows a user to mint 1 NFT per transaction after the presale has ended.
-     */
     function mint() public payable onlyWhenNotPaused {
         require(
             presaleStarted && block.timestamp >= presaleEnded,
@@ -92,25 +70,17 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         _safeMint(msg.sender, tokenIds);
     }
 
-    /**
-     * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
-     * returned an empty string for the baseURI
-     */
+   
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
-    /**
-     * @dev setPaused makes the contract paused or unpaused
-     */
+    
     function setPaused(bool val) public onlyOwner {
         _paused = val;
     }
 
-    /**
-     * @dev withdraw sends all the ether in the contract
-     * to the owner of the contract
-     */
+   
     function withdraw() public onlyOwner {
         address _owner = owner();
         uint256 amount = address(this).balance;
@@ -118,9 +88,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         require(sent, "Failed to send Ether");
     }
 
-    // Function to receive Ether. msg.data must be empty
     receive() external payable {}
 
-    // Fallback function is called when msg.data is not empty
     fallback() external payable {}
 }
